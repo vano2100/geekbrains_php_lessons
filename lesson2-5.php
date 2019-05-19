ed45f948e3398caa9ce722a78f9b9e415fa4d3b4
Rudenko Ivan <rudenko_ia@outlook.com>
2019-05-20 00:40:11 +0900

    Create lesson2-5.php

---
+++ lesson2-5.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Lesson 2</title>
+  </head>
+  <body>
+    Welcome to my site!
+    <hr>
+    Copyrights <?= date('Y') ?>
+  </body>
+</html>
